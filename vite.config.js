5fab862c3afd85152bbe0f400363cfb7f8083ab2
Alfarizi-alf <androstbbox@gmail.com>
2025-07-07 23:37:16 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // Ganti 'NAMA_REPOSITORI_ANDA' dengan nama repositori GitHub Anda
-  base: '/aplikasi-pps-ai/',
-  plugins: [react()],
-})
