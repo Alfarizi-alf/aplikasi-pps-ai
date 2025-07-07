e97cb4e6403e3e03b5b79823e67ecd9722d40af3
Alfarizi-alf <androstbbox@gmail.com>
2025-07-07 17:07:52 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Ganti 'NAMA_REPOSITORI_ANDA' dengan nama repositori GitHub Anda
-  base: '/NAMA_REPOSITORI_ANDA/', 
+  base: '/aplikasi-pps-ai/',
   plugins: [react()],
 })
