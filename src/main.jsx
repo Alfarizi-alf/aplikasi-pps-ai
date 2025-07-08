813e00d8e78c050140583560543e069a1345ba38
Alfarizi-alf <androstbbox@gmail.com>
2025-07-08 10:49:34 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
-
-Run cat ./dist/index.html
 <!DOCTYPE html>
 <html lang="id">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>PPS Berbasis AI</title>
-    <!-- Tautan CSS ditambahkan langsung di sini -->
-    <script type="module" crossorigin src="/aplikasi-pps-ai/assets/index-DypnglQZ.js"></script>
-    <link rel="stylesheet" crossorigin href="/aplikasi-pps-ai/assets/index-C6G_3qQV.css">
   </head>
   <body>
     <div id="root"></div>
+    <!-- Cukup panggil main.jsx. Vite akan menangani sisanya. -->
+    <script type="module" src="/src/main.jsx"></script>
   </body>
 </html>
