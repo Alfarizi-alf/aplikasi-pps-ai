19c7e3c37c906124858dca779c8a935fabe1148a
Alfarizi-alf <androstbbox@gmail.com>
2025-07-08 10:37:59 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-// Baris 'import ./style.css' telah dihapus dari sini
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+Run cat ./dist/index.html
+<!DOCTYPE html>
+<html lang="id">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PPS Berbasis AI</title>
+    <!-- Tautan CSS ditambahkan langsung di sini -->
+    <script type="module" crossorigin src="/aplikasi-pps-ai/assets/index-DypnglQZ.js"></script>
+    <link rel="stylesheet" crossorigin href="/aplikasi-pps-ai/assets/index-C6G_3qQV.css">
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
